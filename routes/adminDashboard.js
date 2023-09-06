22419cdc9a85bbce116ab1e5130cfdac210c208d
nischal <nischalkshaj5@gmail.com>
2023-09-07 01:35:32 +0530

    new changes are made

--- routes/adminDashboard.js
+++ routes/adminDashboard.js
@@ -1,26 +1,24 @@
 // importing the modules
 const express = require('express')
-const Users = require('../models/mongodb')
+const collection = require('../models/mongodb')
 
 const router = express.Router()
 
 // getting all the users
+const credentials ={
+    name : 'Nischal'
+}
 router.post('/',(req, res)=>{
-    Users.find()
+    collection.find()
     .exec()
     .then(users=>{
-        res.render('adminDashboard',{users :users })
+        res.render('adminDashboard',{credentials,users :users })
     })
     .catch (err=>{
         console.error('Error querying users:', err);
         res.status(500).send('Internal Server Error');
     })
 })
-
-// setting up the middleware
-// router.post('/',(req ,res)=>{
-//     res.render('adminDashboard')
-// })
 
 router.post('/add',(req, res)=>{
     res.redirect('/signup')
